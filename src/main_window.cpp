1fbd1f1f4cbdbf242de8229d5c60b4ac27bdb75e
EthsonLiu <ethsonliu@outlook.com>
2020-02-09 20:07:43 +0800

    feat: add Preference dialog

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1,8 +1,35 @@
 #include "main_window.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
+    setWindowTitle("Dukto 7.0.0");
+
+    createPreferenceDialog();
+    createMenuBar();
+    setCentralWidget(createMainWidget());
+}
+
+void MainWindow::createPreferenceDialog()
+{
+    m_preferenceDialog = new QDialog(this);
+    m_preferenceDialog->setWindowTitle(tr("Preference"));
+}
+
+void MainWindow::createMenuBar()
+{
+    QMenu* fileMenu = menuBar()->addMenu(tr("File"));
+    QAction* preferenceAction = new QAction(tr("Preference"), fileMenu);
+    fileMenu->addAction(preferenceAction);
+    connect(preferenceAction, &QAction::triggered, m_preferenceDialog, &QDialog::exec);
+}
+
+QWidget *MainWindow::createMainWidget()
+{
+    QWidget* mainWidget = new QWidget(this);
+
+    return mainWidget;
 }
 
 
--- src/main_window.h
+++ src/main_window.h
@@ -2,12 +2,29 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMenuBar>
+#include <QDialog>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+
     MainWindow(QWidget *parent = nullptr);
+
+private slots:
+
+
+private:
+
+    void createPreferenceDialog();
+    void createMenuBar();
+    QWidget* createMainWidget();
+
+private:
+
+    QDialog* m_preferenceDialog;
+
 };
 #endif // MAINWINDOW_H
